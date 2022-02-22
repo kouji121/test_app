73574efb0c53b97a720220e319932a211790f5aa
kouji121 <mk.745.0212.tok@gmail.com>
2022-02-22 15:45:36 +0900

    20220222 エンティティ追加

--- src/main/java/com/test_app/springboot/StartController.java
+++ src/main/java/com/test_app/springboot/StartController.java
@@ -1,18 +1,39 @@
 package com.test_app.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+import com.test_app.springboot.repositories.StartDataRepository;
+
 
 @Controller
 public class StartController {
 	
+	@Autowired
+	StartDataRepository repository;
+	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
-		mav.addObject("msg","氏名を入力してください。");
+		Iterable<StartData> list = repository.findAll();
+		mav.addObject("data",list);
+		return mav;
+	}
+	@RequestMapping("/other")
+	public String other() {
+		return "redirect:/";
+	}
+	@RequestMapping("/{id}")
+	public ModelAndView index(@PathVariable int id, ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("id", id);
+		mav.addObject("check", id % 2 == 0);
+		mav.addObject("trueVal", "Even number!");
+		mav.addObject("falseVal", "Odd number...");
 		return mav;
 	}
 	@RequestMapping(value="/", method=RequestMethod.POST)
---
+++ src/main/java/com/test_app/springboot/StartData.java
@@ -0,0 +1,50 @@
+package com.test_app.springboot;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="startdata")
+
+public class StartData {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
+	private long id;
+	
+	@Column(length = 50, nullable = false)
+	private String name;
+	
+	@Column(nullable = true)
+	private Integer age;
+
+	public long getid() {
+		return id;
+	}
+	
+	public void setid(long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Integer getAge() {
+		return age;
+	}
+	
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+}
+
+
